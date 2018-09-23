ef8c39c629deb987fbdb5d55da16b9dd49d71c54
Vladislav Gerasimov <gerasimov.v88@gmail.com>
2018-09-23 20:27:57 +0300

    Initial commit

---
+++ src/main/java/ru/vladigeras/masterpizzasale/MasterpizzaSaleApplication.java
@@ -0,0 +1,12 @@
+package ru.vladigeras.masterpizzasale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MasterpizzaSaleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MasterpizzaSaleApplication.class, args);
+	}
+}
